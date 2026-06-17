190a0f42458ec4352c4f2ea452f9b8259baf5efb
Gadzik <oshawot123@gmail.com>
2026-06-16 22:24:45 -0500

    Improved Box Spawning
    Well, kinda. It's still not perfect and I need a better way of detecting the player that is reliable no matter what I do, but it'll work so long as I leave the level root nodes as Node2D

--- Scripts/box_body.gd
+++ Scripts/box_body.gd
@@ -1,7 +1,7 @@
 extends RigidBody2D
 
-@onready var player = get_node("../Player")
-@export var button = Node2D
+@onready var player = get_node("/root/Node2D/Player")
+@onready var button = get_node("../..")
 
 var respawn_timer = 0
 var box_speed = 0
@@ -11,31 +11,6 @@ func _physics_process(_delta: float) -> void :
 		box_speed = player.DASH_SPEED * 2
 	else:
 		box_speed = player.SPEED * 2
-
-	#if respawn_timer < 30 and respawn_timer > 10:
-		#PhysicsServer2D.body_set_state(self, PhysicsServer2D.BODY_STATE_TRANSFORM, box_spawn.transform)
-#
-	#if respawn_timer < 5 and respawn_timer > 0:
-		#$Wall_Box.set_disabled(false)
-		#$Slip_Box.set_disabled(false)
-		#self.set_freeze_enabled(false)
-#
-	#if respawn_timer <= 0:
-		#self.visible = true
-		#$Area2D / Player_Touch.set_disabled(false)
-		#$Area2D2 / Hurt.set_disabled(false)
-#
-	#if respawn_timer > 0:
-		#respawn_timer -= 1
-#
-#func kill_box():
-	#self.visible = false
-	#self.set_freeze_enabled(true)
-	#$Wall_Box.set_disabled(true)
-	#$Slip_Box.set_disabled(true)
-	#$Area2D / Player_Touch.set_disabled(true)
-	#$Area2D2 / Hurt.set_disabled(true)
-	#respawn_timer = 60
 
 func _on_area_2d_body_entered(_body: Node2D) -> void :
 	apply_force(Vector2(box_speed * player.facing_dir, 0))
--- Scripts/box_button.gd
+++ Scripts/box_button.gd
@@ -1,22 +1,27 @@
 extends Sprite2D
 
-@export var box = Node2D
 @onready var load_box = preload("res://Level_Assets/Harmless/box.tscn")
-@export var box_spawn = Node
+@onready var box_spawn = $BoxSpawn
+var box
+var respawn_timer = 0
+
+func _ready() -> void:
+	call_deferred("spawn_box")
+
+func _process(_delta: float) -> void:
+	if respawn_timer == 1:
+		call_deferred("spawn_box")
+	if respawn_timer > 0:
+		respawn_timer -= 1
 
 func spawn_box():
 	box = load_box.instantiate()
-	print("NewBox: ", box)
-	print("PreloadedBox: ", load_box)
-	self.add_child(box)
-	PhysicsServer2D.body_set_state(box, PhysicsServer2D.BODY_STATE_TRANSFORM, box_spawn.transform)
+	box_spawn.add_child(box)
 
 func _on_area_2d_area_entered(_area: Area2D) -> void :
 	$AnimationPlayer.play.call_deferred("Press")
-	spawn_box()
+	call_deferred("kill_box")
 
 func kill_box():
+	respawn_timer = 60
 	box.queue_free.call_deferred()
-	box = load_box
-	var new_box = box.instantiate()
-	PhysicsServer2D.body_set_state(new_box, PhysicsServer2D.BODY_STATE_TRANSFORM, box_spawn.transform)
